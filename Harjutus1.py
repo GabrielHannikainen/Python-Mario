453ecf3011a33c3b42d9b1f67ea645dfe354d707
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-04 11:22:15 +0300

    Add files via upload

---
+++ Harjutus1.py
@@ -0,0 +1,16 @@
+# Gabriel Hännikäinen
+# 04.10.24
+# Harjutus 01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^')
+#int täisarv 123
+#str sõne "tekst"
+#float komaarv 1.23
+
+a = int(input("lisa ruudu külje pikkus: "))
+s = a ** 2 #astendamine 
+print("Vastus: Ruudu pindala on ",s)
