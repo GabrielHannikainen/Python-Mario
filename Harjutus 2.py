ffee8f8b072131c7f8b8083ef7023760fa1281a5
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-04 11:41:43 +0300

    Add files via upload

---
+++ Harjutus 2.py
@@ -0,0 +1,15 @@
+#Harjutus 2
+#Gabriel_Hannikainen
+#04.10.24
+
+
+# 9.Arvusüsteem
+
+
+#kütusekulu arvutamine
+liiter = int(input("Lisa kütusekulu: "))
+print(bin(arv))
+print(hex(arv))
+km = int(input("Lisa läbitud kilomeetrid"))
+kytusekulu = liiter/(km/100)
+print("Sinu keskmine kütusekulu on: ",kytusekulu)
