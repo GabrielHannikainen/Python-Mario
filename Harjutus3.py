e6643970c6e8160d981c67e18a206e0a26449cc4
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-16 11:28:57 +0300

    Create Harjutus3.py

---
+++ Harjutus3.py
@@ -0,0 +1,45 @@
+#16.19.24 Metshein Gabriel
+#Ülesanne 03
+
+#Ülesanne 3.6 Kolmnurk
+#import turtle
+"""
+turtle.speed("fastest")
+turtle.color("blue")
+
+
+for i in range(3):
+    for i in range(3):
+        turtle.forward(100)
+        turtle.left(120)
+
+    turtle.penup()
+    turtle.forward(100*1.5)
+    turtle.pendown()
+
+
+
+
+turtle.done()
+"""
+"""
+#yl 3.5 unistuste auto
+mark, mudel = "bmw", "118"
+aasta = 1997
+hind = 136,60
+auto = mark+" "+mudel
+print(“Minu unistuste auto on",auto,"aastast",aasta,", mille hind on",hind," eurot.”)
+"""
+
+
+#ülesanne 3.4 Lemmik Raamat
+raamatu_pealkiri = "Kevade"
+raamatu_autor = "Oskar Luts"
+raamat = raamatu_pealkiri+" "+raamatu_autor
+lehekylgede_arv = 360
+hindamisskoor = 9,8
+
+
+
+
+#ülesanne 3.2 Ostunimekiri
