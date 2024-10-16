18b93ba795ef49aa33d231e74af734fae7100453
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-16 11:45:50 +0300

    Create Harjutus4.py

---
+++ Harjutus4.py
@@ -0,0 +1,15 @@
+#Harjutus4
+
+#1.Aia pikkus
+"""
+a = int(input("Sisesta külg 1 : "))
+b = int(input("Sisesta külg 2 : "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus on {p} meetrit.")
+"""
+#2. Raamatute allahindlus
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus
+print(f"{kogus}sfsgs ada ada ada {summa:0.2f}£")
