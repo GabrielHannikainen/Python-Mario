da876ef6d16856e444fd9cb6ea948f4e3f03d0da
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-18 11:43:53 +0300

    Create Harjutus.6.py

---
+++ Harjutus.6.py
@@ -0,0 +1,22 @@
+#Harjutus 6
+#Gabriel
+#18.10.24
+
+import turtle
+import math
+
+kordaja = 10
+a = 53
+h = 4.4
+x = abs(h / math.tan(a))
+c = math.sqrt(x**2 + h**2)
+print(x)
+turtle.forward(x*kordaja)
+turtle.left(90)
+turtle.forward(h*kordaja)
+turtle.left(180-66)
+turtle.forward(c*kordaja)
+
+
+
+turtle.done()
