415055ff7fa3dde4aef64ad32627142dc4e1505d
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-10-18 11:46:03 +0300

    Update Harjutus.6.py

--- Harjutus.6.py
+++ Harjutus.6.py
@@ -6,7 +6,7 @@ import turtle
 import math
 
 kordaja = 10
-a = 53
+a = math.radians(53)
 h = 4.4
 x = abs(h / math.tan(a))
 c = math.sqrt(x**2 + h**2)
@@ -14,7 +14,7 @@ print(x)
 turtle.forward(x*kordaja)
 turtle.left(90)
 turtle.forward(h*kordaja)
-turtle.left(180-66)
+turtle.left(180-37)
 turtle.forward(c*kordaja)
 
 
