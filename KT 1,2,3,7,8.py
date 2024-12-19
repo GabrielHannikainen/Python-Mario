c97e7b2674d3aa6d4f389e51a0123ca3761a46a5
GabrielHannikainen <gabriel@hkhk.edu.ee>
2024-12-19 15:51:28 +0200

    Update KT 1,2,3,7,8.py

--- KT 1,2,3,7,8.py
+++ KT 1,2,3,7,8.py
@@ -1,49 +1,44 @@
 #1 Korrutamise ülesanne Gabriel
-"""
 import random
 
-def korrutus_tehe():
-   nr1 = random.randint(1, 15)
-   nr2 = random.randint (1, 15)
+nr1 = random.randint(1, 15)
+nr2 = random.randint (1, 15)
 
-   print(f"Kui palju on {nr1} * {nr2}?")
+print(f"Kui palju on {nr1} * {nr2}?")
 
-   vastus = int(input("vastus siia: "))
+vastus = int(input("vastus siia: "))
 
-   if vastus == nr1 * nr2:
-        print("õige vastus!")
-    else:
-        print(f"vale, õige vastus {nr1} * {nr2}")
+if vastus == nr1 * nr2:
+    print("õige vastus!")
+else:
+    print(f"vale, õige vastus {nr1} * {nr2}")
 
-for i in range(10):
-    korrutus_tehe()
-"""
+
 #2 Vanused Gabriel
-"""
-def vanused():
-    vanused = [37, 24, 16, 19, 47, 11, 55]
 
-    suurim = max(vanused)
-    vaikseim = min(vanused)
-    kogusumma = sum(vanused)
-    keskmine = kogusumma/ len(vanused)
+# vanused = [37, 24, 16, 19, 47, 11, 55]
 
-    print(f"vanim: {suurim}")
-    print(f"noorim: {vaikseim}")
-    print(f"kogu vanused: {kogusumma}")
-    print(f"keskmine: {keskmine}")
-"""
-#3 Positiivsed ja Negatiivsed Gabriel
-"""
-positiivsed = []
-negatiivsed = []
-for i in range(5):
-    arv = int(input(f"sisesta {i+1}arv : "))
-    if arv > 0:
-        positiivsed.append[]
-    elif arv < 0:
-        negatiivsed.append[]
-    print(f"positiivsed arvud:{positiivsed}")
-    print(f"negatiivsed arvud: {negatiivsed}")
-"""
-print("hello world")
+# suurim = max(vanused)
+# vaikseim = min(vanused)
+# kogusumma = sum(vanused)
+# keskmine = kogusumma/ len(vanused)
+
+# print(f"vanim: {suurim}")
+# print(f"noorim: {vaikseim}")
+# print(f"kogu vanused: {kogusumma}")
+# print(f"keskmine: {keskmine}")
+
+# #3 Positiivsed ja Negatiivsed Gabriel
+
+# positiivsed = []
+# negatiivsed = []
+
+# for i in range(5):
+#     arv = int(input(f"Sisesta {i+1}. arv: "))
+#     if arv > 0:
+#         positiivsed.append(arv)
+#     elif arv < 0:
+#         negatiivsed.append(arv)
+
+# print(f"Positiivsed arvud: {positiivsed}")
+# print(f"Negatiivsed arvud: {negatiivsed}")
