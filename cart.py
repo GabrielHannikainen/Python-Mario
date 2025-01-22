adf614a1a25c4bbaf777e5015cff38ec84ae3914
GabrielHannikainen <gabriel@hkhk.edu.ee>
2025-01-22 20:47:56 +0200

    Update cart.py

--- cart.py
+++ cart.py
@@ -2,12 +2,32 @@
 # Kasutaja sisestab otsitava
 # Kuvab otsitavad põhiandmed (2-3tk)
 
+
+
 import json
+import requests  # Raamatukogu HTTP-päringute jaoks
 
-
-with open('carts.json', 'r', encoding='utf-8') as file:
-    carts = json.load(file)
-
-print(carts)
-
+url = "https://dummyjson.com/carts"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+otsitav = input("Sisesta otsitav toote nimi: ").lower()
+results = []
+for cart in data['carts']:
+    for product in cart['products']:
+        if otsitav in product['title'].lower():
+            results.append({
+                "Title": product['title'],
+                "Price": product['price'],
+                "Quantity": product['quantity']
+            })
+            
+if results:
+    print("\nLeitud tulemused:")
+    for result in results[:3]:  # Kuvab kuni 3 tulemust
+        print(f"Toote nimi: {result['Title']}")
+        print(f"Hind: {result['Price']} €")
+        print(f"Kogus: {result['Quantity']}")
+else:
+    print("Tulemusi ei leitud.")
 
